00255d1804e959dfeaf4747cda5601dd4ac4a352
guangdaji <410082155@qq.com>
2025-11-29 01:34:59 +0800

    Add files via upload

---
+++ 蓝山lesson5作业/lv2/handout/handout.go
@@ -0,0 +1,45 @@
+package handout
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Control 线程开启，关闭，锁，线程数量,任务数量
+type Control struct {
+	Lock       sync.Mutex
+	wg         sync.WaitGroup
+	goroutines int
+	tasks      int
+	ch         chan func()
+}
+
+func (C *Control) Begin(g int, tasks int) {
+	if g < 10 {
+		g = 10
+	}
+	C.goroutines = g
+	C.tasks = tasks
+	C.ch = make(chan func(), C.tasks)
+}
+func (C *Control) Gorun() {
+	for range C.goroutines {
+		C.wg.Add(1)
+		go func() {
+			C.Lock.Lock()
+			defer C.wg.Done()
+			for i := range C.ch {
+				i()
+			}
+			C.Lock.Unlock()
+		}()
+	}
+}
+func (C *Control) Input(f func()) {
+	C.ch <- f
+}
+func (C *Control) Close() {
+	close(C.ch)
+	C.wg.Wait()
+	fmt.Println("已结束")
+}
